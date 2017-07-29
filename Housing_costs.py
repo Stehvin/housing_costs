62f4434563fd3045424e6221dbb1545d78fe2865
Stehvin <stehvin.olson@gmail.com>
2017-07-28 19:47:44 -0500

    Add two more input variables along with additional documentation

--- Housing_costs.py
+++ Housing_costs.py
@@ -1,19 +1,34 @@
 # -*- coding: utf-8 -*-
 
+# input parameters
 Loan = 75000
 DownPayment = 15000
 AnnualInterestRate = 0.035
 StockMarketRate = 0.06
 LoanLength = 120
 CurrentTimeYears = 1
+RentPrice = 800
+HouseSellValue = 0.85
 
 def FixedMonthlyPayment(UnpaidLoanAmount, AnnualInterestRate, Months):
+    """Calculate the fixed monthly loan payment.
+    Formula: P = L[c(1 + c)n]/[(1 + c)n - 1]
+    Where P = monthly payment, L = original loan amount, 
+    n = loan length in months, and c = monthly interest rate.
+    """
     MonthlyRate = AnnualInterestRate/12    
     numerator = UnpaidLoanAmount * MonthlyRate * ((1 + MonthlyRate)**Months)
     denomenator = ((1 + MonthlyRate)**Months) - 1
     return numerator/denomenator
 
-def RemainingBalance(UnpaidLoanAmount, AnnualInterestRate, Months, MonthsPaid):
+def RemainingBalance(UnpaidLoanAmount, AnnualInterestRate, Months, 
+                     MonthsPaid):
+    """Calculate the remaining loan balance.
+    Formula: B = L[(1 + c)n - (1 + c)p]/[(1 + c)n - 1]
+    Where B = remaining balance, L = original loan amount, 
+    n = loan length in months, p = months passed, 
+    and c = monthly interest rate.
+    """
     MonthlyRate = AnnualInterestRate/12    
     numerator = UnpaidLoanAmount * (((1 + MonthlyRate)**Months) - \
                 ((1 + MonthlyRate)**MonthsPaid))
@@ -21,11 +36,21 @@ def RemainingBalance(UnpaidLoanAmount, AnnualInterestRate, Months, MonthsPaid):
     return numerator/denomenator
     
 def OpportunityCost(DownPayment, fixed_monthly_payment, StockMarketRate,
-                    CurrentTimeYears): 
-    balance = 0    
-    YearNet = (800 - fixed_monthly_payment) * 12   
+                    CurrentTimeYears, RentPrice): 
+    """Calculate the opportunity cost of investment revenue and monthly 
+    housing expense compared to renting.
+    """
+    balance = 0
+    
+    # monthly housing expense compared to renting,
+    # could be positive or negative
+    YearNet = (RentPrice - fixed_monthly_payment) * 12
+    
+    # renting vs buying opportunity gain/cost
     for i in range(CurrentTimeYears):
         balance = (balance + YearNet)*(1+StockMarketRate)
+    
+    # total opporunity cost includes down payment opportunity cost
     Total_Cost = ((DownPayment*((1+StockMarketRate)**CurrentTimeYears)) * \
                     -1) + balance
     return Total_Cost
@@ -34,9 +59,11 @@ MonthlyPay = FixedMonthlyPayment(Loan - DownPayment, AnnualInterestRate,
                                  LoanLength)
 LeftToPay = RemainingBalance(Loan - DownPayment, AnnualInterestRate, LoanLength,
                              CurrentTimeYears*12)
-OppCost = OpportunityCost(DownPayment, MonthlyPay, StockMarketRate, CurrentTimeYears)
+OppCost = OpportunityCost(DownPayment, MonthlyPay, StockMarketRate, 
+                          CurrentTimeYears, RentPrice)
 print('Monthly Payment: ' + str(MonthlyPay))
 print('RemainingBalance: ' + str(LeftToPay))
-print('Opportunity Costs (Assuming $800 rent): ' + str(OppCost))
+print('Opportunity Costs (Assuming $' + str(RentPrice) + ' rent): ' + 
+      str(OppCost))
                                               
-print('Net Gain/Loss: ' + str((Loan*.85) - LeftToPay + OppCost))
+print('Net Gain/Loss: ' + str((Loan*HouseSellValue) - LeftToPay + OppCost))
